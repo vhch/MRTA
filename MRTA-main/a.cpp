267d7a1ed4ce9e655edd1de3a2bbfa273b67833a
sj <vhch66@gmail.com>
2022-06-16 14:04:25 +0900

    1

---
+++ MRTA-main/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+
+
+int main(void){
+    
+    int a=-2;
+    std::cout << std::abs(a) <<endl;
+    
+    return 0;
+} 
